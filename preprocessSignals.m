7aba6eb5770e45363a9078437026b3234ca23f7c
Julian Weaver <114316166+weavejul@users.noreply.github.com>
2024-04-03 17:42:48 -0500

    Delete preprocessSignals.m

--- preprocessSignals.m
+++
@@ -1,5 +0,0 @@
-%% load gdf files
-[signal, header] = sload("Subject1_OfflineRest.gdf");
-
-% subject -> off/online -> session -> run resting/trials + header
-% FeatureStore1 = struct('offline', {cell(6, 1)}, 'online', {cell(6, 1)});
