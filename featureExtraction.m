aa001545fdf8ac03333286bcb961f2fc48217dd3
Emerald-Z <ejzhang2004@gmail.com>
2024-04-15 15:47:54 -0500

    features

--- featureExtraction.m
+++ featureExtraction.m
@@ -1,3 +1,2 @@
-%% extract features
-% time frequency decomposition - psd for each channel in sliding window
-% 0.5s and step 1/16 s use welch -> ERD/ERS calculated
+%% call extract features
+% implement PCA
