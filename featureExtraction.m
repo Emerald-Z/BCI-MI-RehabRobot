5cfb8a34f1ea3400399c9718708b209e05ec5a12
Nihita-Sarma <nihitasarma@utexas.edu>
2024-04-17 08:39:51 -0500

    pca fixed online +offline

--- featureExtraction.m
+++ featureExtraction.m
@@ -3,10 +3,21 @@
 %runs.eeg{1}{j} <-- 1 = run #, j = sample number
 runs = Subject1.offline.runs;
 offline_feats = cell(3,1);
+feats_used = zeros(1, 8);
+
+%save('avg_coeffs.mat', 'avg_coeffs');
+%save('avg_mean.mat', 'avg_mean');
+avg_coeffs = load('avg_coeffs.mat').avg_coeffs;
+avg_mean = load('avg_mean.mat').avg_mean;
+
+feat_indices = [2, 5, 3, 6, 7];
+%avg_coeffs = zeros(7,7);
+%avg_mean = zeros(1,7);
 
 for i=1:3
     feats = cell(20, 1);
     featsR = cell(20, 1);
+    feats_fixed = cell(20,1);
     for j=1:20
         filteredSignal = runs.eeg{i}{j};
         window_size = 0.2;
@@ -24,7 +35,11 @@ for i=1:3
         featureMatrix = zscore(featureMatrix);
         
         % Perform PCA
-        [coeff, score, latent, tsquared, explained] = pca(featureMatrix);
+        [coeff, score, latent, tsquared, explained, mu] = pca(featureMatrix);
+        %disp(size(mu))
+
+        X_centered = bsxfun(@minus, featureMatrix, avg_mean); 
+        new_feats = X_centered * avg_coeffs;
         
         % Plot the explained variance to see how many components to retain
         % figure;
@@ -41,23 +56,43 @@ for i=1:3
 
         % Extract the top scoring features based on principal components
         top_features = coeff(:, 1:numComponents);
+
+        %disp(size(featureMatrix))
         
         % Find indices of highest scoring features
         [~, sorted_indices] = sort(sum(abs(top_features), 2), 'descend');
-        disp(size(featureMatrix(:, sorted_indices(1:4))));
-        feats{j} = model_input_feats(:, 1:3);
+        disp(sorted_indices)
+        %for ind=1:5
+        %    feats_used(sorted_indices(ind)) = feats_used(sorted_indices(i)) + 1;
+        %end
+        %disp(size(featureMatrix(:, sorted_indices(1:4))));
+        feats{j} = model_input_feats(:, 1:4);
         featsR{j} = featureMatrix(:, sorted_indices(1:4));
+        feats_fixed{j} = new_feats(:, feat_indices);
+        %avg_coeffs = avg_coeffs + coeff;
+        %avg_mean = avg_mean + mu;
     end
-    offline_feats{i} = featsR;
+    %disp("HERE")
+    %disp(size(feats_fixed{1}))
+    %disp(size(featsR{1}))
+    offline_feats{i} = feats_fixed;
 end
+
+%avg_coeffs = avg_coeffs/60;
+%avg_mean = avg_mean/60;
+
+%disp(feats_used)
+%disp(avg_coeffs)
+%disp(size(offline_feats))
 
 for i=1:4
     online_feats = cell(3,1);
     for  j=1:20
         feats = cell(20, 1);
+        feats_fixed = cell(20,1);
         filteredSignal = session2.eeg{i}{j};
-        window_size = 5;
-        fs = 1;
+        window_size = 0.2;
+        fs = 512;
         label = session2.labels.type{i}(j);
         overlap = 0.75;
         
@@ -72,6 +107,9 @@ for i=1:4
         
         % Perform PCA
         [coeff, score, latent, tsquared, explained] = pca(featureMatrix);
+
+        X_centered = bsxfun(@minus, featureMatrix, avg_mean); 
+        new_feats = X_centered * avg_coeffs;
         
         % Plot the explained variance to see how many components to retain
         % figure;
@@ -82,10 +120,12 @@ for i=1:4
         
         % Choose components that explain, e.g., at least 95% of the variance
         cumVar = cumsum(explained);
-        numComponents = find(cumVar >= 95, 1, 'first');
-        model_input_feats = score(:, 1:numComponents);
-        disp(size(model_input_feats))
+        %numComponents = find(cumVar >= 95, 1, 'first');
+        model_input_feats = score(:, 1:4);
+        [sortedLoadings, featureIdx] = sort(abs(coeff(:,1)), 'descend');
+        %disp(size(model_input_feats))
         feats{j} = model_input_feats;
+        feats_fixed{j} = new_feats(:, feat_indices);
     end
-    online_feats{i} = feats;
+    online_feats{i} = feats_fixed;
 end
